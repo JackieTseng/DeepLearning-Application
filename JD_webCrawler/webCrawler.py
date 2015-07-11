81b5f8d72e49b3aa3f2f35f06d3dc1790191c259
JackieTseng <jackietseng86@gmail.com>
2015-07-11 18:55:25 +0800

    JD web crawler update

--- JD_webCrawler/webCrawler.py
+++ JD_webCrawler/webCrawler.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import sys, requests, bs4, urllib
 
 #reload(sys)
@@ -6,15 +7,79 @@ import sys, requests, bs4, urllib
 FILECOUNTER = 1
 ATTRS = {}
 
-# Write a page in jd.com(woman-bag)
-def getItemInfo(url):
+def getItemInfoByOne(number, color, url):
     global FILECOUNTER
     global ATTRS
     response = requests.get(url)
-    #output = open('womanbag.html', 'w')
-    #output.write(response.content)
-    #output.close()
     soup = bs4.BeautifulSoup(response.content, from_encoding="utf-8")
+
+    attrs_list = [()]
+    # download img
+    pic_url = soup.select('div#preview img')
+    pic_src = pic_url[0].attrs.get('src')
+    filetype = pic_src[pic_src.rfind('.'):]
+    req = requests.get(pic_src)
+    pic_output = open(str(FILECOUNTER) + '_' + str(number) + filetype, 'wb')
+    pic_output.write(req.content)
+
+    # download attrs in li
+    param_list = soup.select('ul#parameter2 > li')
+    if len(param_list) == 0:
+        param_list = soup.select('div#product-detail ul.detail-list li')
+    param_str_list = [a.string for a in param_list]
+    param_output = open(str(FILECOUNTER) + '_' + str(number) + '.txt', 'wb')
+    for i in param_str_list:
+        if i != None:
+            pos = i.find(u'\uff1a')
+            key = i[:pos].encode('utf-8')
+            if key not in ATTRS:
+                ATTRS[key] = 1
+            else:
+                ATTRS[key] += 1
+            value = i[pos + 1:].encode('utf-8')
+            if key == (u'\u989c\u8272').encode('utf-8'):
+                value = color
+            attrs_list.append((key, value))
+            #param_output.write(key + '\t' + value + '\n')
+
+    # download attrs in table
+    table = soup.find('table', attrs={'class':'Ptable'})
+    if table != None:
+        #table = soup.select('div#product-detail-2 > table.Ptable')
+        #td = soup.select('td')
+        td = table.find_all('td')
+        i = 0
+        counter = len(td)
+        while i < counter:
+            #while td[i] == None or td[i].string is None or len((td[i].string).strip()) == 0:
+            #    i+= 1
+            key = td[i].string.encode('utf-8')
+            if key not in ATTRS:
+                ATTRS[key] = 1
+            else:
+                ATTRS[key] += 1
+            value = td[i + 1].string.encode('utf-8')
+            attrs_list.append((key, value))
+            #param_output.write(key + '\t' + value + '\n')
+            i += 2
+    temp = set()
+    # write url
+    length = len(attrs_list)
+    for i in range(1, length):
+        if attrs_list[i][0] in temp:
+            continue
+        temp.add(attrs_list[i][0])
+        param_output.write(attrs_list[i][0] + '\t' + attrs_list[i][1] + '\n')
+    param_output.write('url'.encode('utf-8') + '\t' + url.encode('utf-8'))
+    param_output.close()
+
+def getItemInfoWithNoColor(url):
+    global FILECOUNTER
+    global ATTRS
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content, from_encoding="utf-8")
+
+    attrs_list = [()]
     # download img
     pic_url = soup.select('div#preview img')
     pic_src = pic_url[0].attrs.get('src')
@@ -38,29 +103,60 @@ def getItemInfo(url):
             else:
                 ATTRS[key] += 1
             value = i[pos + 1:].encode('utf-8')
-            param_output.write(key + '\t' + value + '\n')
+            attrs_list.append((key, value))
+            #param_output.write(key + '\t' + value + '\n')
 
     # download attrs in table
-    if soup.select('ul.m-tab-trigger li#detail-tab-param') != None:
-        table = soup.select('div#product-detail-2 > table.Ptable')
-        td = soup.select('td')
+    table = soup.find('table', attrs={'class':'Ptable'})
+    if table != None:
+        #table = soup.select('div#product-detail-2 > table.Ptable')
+        #td = soup.select('td')
+        td = table.find_all('td')
         i = 0
         counter = len(td)
         while i < counter:
-            while td[i] == None or td[i].string is None or len((td[i].string).strip()) == 0:
-                i+= 1
+            #while td[i] == None or td[i].string is None or len((td[i].string).strip()) == 0:
+            #    i+= 1
             key = td[i].string.encode('utf-8')
             if key not in ATTRS:
                 ATTRS[key] = 1
             else:
                 ATTRS[key] += 1
             value = td[i + 1].string.encode('utf-8')
-            param_output.write(key + '\t' + value + '\n')
+            attrs_list.append((key, value))
+            #param_output.write(key + '\t' + value + '\n')
             i += 2
-
+    temp = set()
     # write url
+    length = len(attrs_list)
+    for i in range(1, length):
+        if attrs_list[i][0] in temp:
+            continue
+        temp.add(attrs_list[i][0])
+        param_output.write(attrs_list[i][0] + '\t' + attrs_list[i][1] + '\n')
     param_output.write('url'.encode('utf-8') + '\t' + url.encode('utf-8'))
     param_output.close()
+
+# Write a page in jd.com(woman-bag)
+def getItemInfo(url):
+    global FILECOUNTER
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content, from_encoding="utf-8")
+
+    # download other colors
+    colorUrls = [()]
+    diffColor = soup.select('div.dd > div.item')
+    for i in diffColor:
+        cur = i.find('a')
+        if cur.find('img') != None:
+            colorUrls.append((cur.find('i').string.encode('utf-8'), cur.attrs.get('href')))
+    
+    length = len(colorUrls)
+    if length == 1:
+        getItemInfoWithNoColor(url)
+    else:
+        for i in range(1, length):
+            getItemInfoByOne(i, colorUrls[i][0], colorUrls[i][1])
     FILECOUNTER += 1
 
 # Get all the urls in the current web page
@@ -108,4 +204,4 @@ if __name__ == "__main__":
     storeAttrs(ATTRS)
     '''
     #getItemInfo('http://item.jd.com/1037568901.html')
-    getItemInfo('http://item.jd.com/1505812327.html')
+    getItemInfo('http://item.jd.com/1304570.html')
