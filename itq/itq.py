993621656b09716df43822ef7b001d59695f401f
JackieTseng <jackietseng86@gmail.com>
2015-07-21 12:01:39 +0800

    comment in ITQ

--- itq/itq.py
+++ itq/itq.py
@@ -5,24 +5,44 @@ import os, sys
 import numpy as np
 from pca import *
 
+# Input: 
+#       V: n*c PCA-processed matrix(n is the number of sample
+#          and c is the code length)
+#       n: the number of iterations(50 is enough)
+# Output:
+#       B: n*c binary matrix
+#       R: the rotation found by ITQ
 def itq(V, n):
+    # initialize with a orthogonal random rotation matrix R
     (number, bit) = V.shape
+    # Gaussian distribution of mean 0 and variance 1
     R = np.random.randn(bit, bit)
     U, V2, S2 = np.linalg.svd(R)
     R = U[:, range(0, bit)]
+    
+    # Fix and Update iterations
     for i in range(n):
+        # Fix R and update B(UX)
         Z = V * R
         (row, col) = Z.shape
         UX = np.ones((row, col)) * -1
         UX[Z >= 0] = 1
+        
+        # Fix B and update R
         C = UX.T * V
         UB, sigma, UA = np.linalg.svd(C)
         R = UA * UB.T
     B = UX
+    # Transform into binary code
     B[B < 0] = 0
-    print B
+    #print B
+    return (B, R)
+    
+
+def compressITQ(mx, bit, iters):
+    Y = pca(mx, bit)
+    itq(Y, iters)
 
 if __name__ == '__main__':
     mx = readFile('')
-    Y = pca(mx)
-    itq(Y, 50)
+    compressITQ(mx, 90, 50)
--- itq/pca.py
+++ itq/pca.py
@@ -7,7 +7,6 @@ import numpy as np
 def strListToFlatList(line):
     temp = []
     for i in line:
-        #print i 
         temp.append(float(i))
     return temp
 
@@ -26,6 +25,44 @@ def readFile(fileStr):
     mx = np.mat(data)
     return mx
 
+# Input:
+#       mx: n*c matrix(n is the number of sample
+#           and c is the number of attributes)
+#       bit: the compress length
+def pca(mx, bit):
+    # Step 1 -> center data
+    row, col = mx.shape
+    for i in range(col):
+        mx[:, i] -= mx[:, i].mean()
+
+    # Step 2 -> scatter matrix
+    cov = np.cov(mx, rowvar = 0)
+
+    # Step 3 -> eigenvectors and eigenvalues
+    val, vec = np.linalg.eig(cov)
+
+    # Step 4 -> sort the eigenvectors together with the eigenvalues
+    idx = np.argsort(-val)
+    sort_val = val[idx]
+    sort_vec = vec[:, idx]
+    
+    # Step 5 -> choose K rows of eigenvectors
+    K = bit #128
+    idx = idx[:K]
+    P = sort_vec[:, idx]
+
+    # Step 6 -> calculate the value of contribution (>= 0.85 is accepted)
+    print str(1.0 * sum(sort_val[:K]) / sum(sort_val))
+    
+    # return transform matrix
+    Y = mx * P
+    return Y
+
+if __name__ == '__main__':
+    mx = readFile('C1.txt')
+    pca(mx, 90)
+
+# Other PCA
 def PCA(dataMat, topNfeat=5):
     meanVals = mean(dataMat, axis = 0)
     # 减去均值
@@ -45,33 +82,3 @@ def PCA(dataMat, topNfeat=5):
     lowDDataMat = stded * redEigVects
     reconMat = (lowDDataMat * redEigVects.T) * std(dataMat) + meanVals
     return lowDDataMat, reconMat
-
-def pca(mx):
-    # Step 1 -> center data
-    row, col = mx.shape
-    for i in range(col):
-        mx[:, i] -= mx[:, i].mean()
-
-    # Step 2 -> scatter matrix
-    cov = np.cov(mx, rowvar = 0)
-
-    # Step 3 -> eigenvectors and eigenvalues
-    val, vec = np.linalg.eig(cov)
-
-    # Step 4 -> sort the eigenvectors together with the eigenvalues
-    idx = np.argsort(-val)
-    sort_val = val[idx]
-    sort_vec = vec[:, idx]
-    
-    # Step 5 -> choose K rows of eigenvectors
-    K = 90 #128
-    idx = idx[:K]
-    P = sort_vec[:, idx]
-    
-    # return transform matrix
-    Y = mx * P
-    return Y
-
-if __name__ == '__main__':
-    mx = readFile('C1.txt')
-    pca(mx)
