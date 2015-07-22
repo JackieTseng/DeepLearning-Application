6e9052534301853f1f30c5d7ce5adb4558ca8f93
JackieTseng <jackietseng86@gmail.com>
2015-07-22 15:56:51 +0800

    MIH update

--- multi-index hashing/bitread.cpp
+++ multi-index hashing/bitread.cpp
@@ -8,14 +8,30 @@ using namespace std;
 
 vector<bitset<BITWIDTH> > data;
 
-int main(int argc, const char *argv[]) {
-    fstream file;
-    file.open("B.txt");
+void readData(string);
+int hammingDis(bitset<BITWIDTH>, bitset<BITWIDTH>);
 
+int main(int argc, const char *argv[]) {
+    readData("B.txt");
+    return 0;
+}
+
+void readData(string fileName) {
+    fstream file;
+    file.open(fileName.c_str());
     string line;
     while (getline(file, line)) {
-        data.push_back(bitset<64>(line));
+        data.push_back(bitset<BITWIDTH>(line));
     }
-    cout << data.size();
-    return 0;
+    return;
+}
+
+int hammingDis(bitset<BITWIDTH> a, bitset<BITWIDTH> b) {
+    unsigned long t = (a ^ b).to_ulong();
+    int counter = 0;
+    while (t) {
+        counter += (t & 0x01);
+        t >>= 1;
+    }
+    return counter;
 }
